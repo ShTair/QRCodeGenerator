358cbdeff1a841b1992483b9048b5dd37c781fc9
ShTair <git@vsh.jp>
2018-10-26 16:03:18 +0900

    出力を実装

--- QRCodeGenerator/Program.cs
+++ QRCodeGenerator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShComp;
+using System;
 using System.IO;
 using System.Linq;
 
@@ -12,50 +13,49 @@ namespace QRCodeGenerator
             {
                 var data = File.ReadAllText(arg);
 
+                var ecl = QRCoder.QRCodeGenerator.ECCLevel.L;
+                var dr = false;
+
+                var name = Path.GetFileNameWithoutExtension(arg);
+                var d = name.LastIndexOf("-");
+                if (d != -1 && d + 1 < name.Length)
+                {
+                    foreach (var item in name.Substring(d + 1).ToUpper())
+                    {
+                        switch (item)
+                        {
+                            case 'M': ecl = QRCoder.QRCodeGenerator.ECCLevel.M; break;
+                            case 'Q': ecl = QRCoder.QRCodeGenerator.ECCLevel.Q; break;
+                            case 'H': ecl = QRCoder.QRCodeGenerator.ECCLevel.H; break;
+                            case 'L': ecl = QRCoder.QRCodeGenerator.ECCLevel.L; break;
+                            case 'R': dr = true; break;
+                        }
+                    }
+                }
+
                 var qr = new QRCoder.QRCodeGenerator();
-                var qrcd = qr.CreateQrCode(data, QRCoder.QRCodeGenerator.ECCLevel.L);
+                var qrcd = qr.CreateQrCode(data, ecl);
+                var matrix = qrcd.ModuleMatrix;
+                var width = matrix[0].Count;
+                var height = matrix.Count;
+                var commands = PathCommandGenerator.Generate((x, y) => matrix[y].Get(x), width, height);
 
-                //var ecl = ErrorCorrectionLevel.L;
-                //var dr = false;
+                var path = Path.GetExtension(arg).Equals(".svg", StringComparison.CurrentCultureIgnoreCase) ? arg + ".svg" : Path.ChangeExtension(arg, ".svg");
+                using (var writer = new StreamWriter(path, false))
+                {
+                    writer.WriteLine(@"<?xml version=""1.0"" encoding=""utf-8""?>");
+                    writer.WriteLine($@"<svg xmlns=""http://www.w3.org/2000/svg"" viewBox=""0 0 {width + 8} {height + 8}"">");
+                    writer.Write(@"<path d=""");
 
-                //var name = Path.GetFileNameWithoutExtension(arg);
-                //var d = name.LastIndexOf("-");
-                //if (d != -1 && d + 1 < name.Length)
-                //{
-                //    foreach (var item in name.Substring(d + 1).ToUpper())
-                //    {
-                //        switch (item)
-                //        {
-                //            case 'M': ecl = ErrorCorrectionLevel.M; break;
-                //            case 'Q': ecl = ErrorCorrectionLevel.Q; break;
-                //            case 'H': ecl = ErrorCorrectionLevel.H; break;
-                //            case 'L': ecl = ErrorCorrectionLevel.L; break;
-                //            case 'R': dr = true; break;
-                //        }
-                //    }
-                //}
+                    foreach (var command in commands)
+                    {
+                        writer.Write(command.ToString(4, 4));
+                    }
 
-                //var qre = new QrEncoder(ecl);
-                //var qrc = qre.Encode(data);
-                //var m = qrc.Matrix.InternalArray;
-                //var commands = PathCommandGenerator.Generate((x, y) => m[x, y], qrc.Matrix.Width, qrc.Matrix.Height);
-
-                //var path = Path.GetExtension(arg).Equals(".svg", StringComparison.CurrentCultureIgnoreCase) ? arg + ".svg" : Path.ChangeExtension(arg, ".svg");
-                //using (var writer = new StreamWriter(path, false))
-                //{
-                //    writer.WriteLine(@"<?xml version=""1.0"" encoding=""utf-8""?>");
-                //    writer.WriteLine($@"<svg xmlns=""http://www.w3.org/2000/svg"" viewBox=""0 0 {qrc.Matrix.Width + 8} {qrc.Matrix.Height + 8}"">");
-                //    writer.Write(@"<path d=""");
-
-                //    foreach (var command in commands)
-                //    {
-                //        writer.Write(command.ToString(4, 4));
-                //    }
-
-                //    writer.WriteLine(@"""/>");
-                //    if (dr) writer.WriteLine($@"<rect x=""0"" y=""0"" width=""{qrc.Matrix.Width + 8}"" height=""{qrc.Matrix.Height + 8}"" stroke=""black"" stroke-width=""0.1"" fill=""none"" />");
-                //    writer.WriteLine("</svg>");
-                //}
+                    writer.WriteLine(@"""/>");
+                    if (dr) writer.WriteLine($@"<rect x=""0"" y=""0"" width=""{width + 8}"" height=""{height + 8}"" stroke=""black"" stroke-width=""0.1"" fill=""none"" />");
+                    writer.WriteLine("</svg>");
+                }
             }, () =>
             {
                 Console.WriteLine("ファイルをドロップしてね。");
